94d8d8f2e64118a22bb25db3fe5bf25c948b000c
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2026-09-21 12:33:41 +0330

    deploy(fix): set resource limitation on namespace :pushpin:

--- Dockerfile
+++ Dockerfile
@@ -31,6 +31,4 @@ USER 1000:1000
 
 EXPOSE 8000
 
-CMD ["sh", "-c", "python manage.py collectstatic --noinput && gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 4"]
-
-
+CMD ["sh", "-c", "python manage.py migrate && python manage.py collectstatic --noinput && gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 4"]
--- config/settings.py
+++ config/settings.py
@@ -64,6 +64,9 @@ MIDDLEWARE = [
     "apps.audit.middleware.AuditContextMiddleware",
     
     "django.middleware.security.SecurityMiddleware",
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
